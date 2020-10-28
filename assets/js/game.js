7c043d073c21f1d3e3e5c339a9b317afd7390478
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2020-10-28 15:48:35 -0400

    random health and attack variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,14 +30,16 @@ if (promptFight === "skip" || promptFight === "SKIP") {
 if (confirmSkip) {
     window.alert(playerName + " has decided to skip this fight! Goodbye!");
     //subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 10;
+    playerMoney = Math.max(0, playerMoney - 10);
     console.log("playerMoney", playerMoney);
     break;
    }
 }
 
- // remove enemy's health by subtracting the amount set in the player attack variable
-    enemyHealth = enemyHealth - playerAttack;
+ // generate randome damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0,enemyHealth - damage);
     console.log (
         playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
     );
@@ -53,8 +55,10 @@ if (confirmSkip) {
         window.alert(enemyName + " still has " + enemyHealth + " health points left");
     }
 
-    // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0,playerHealth - damage);
     console.log (
         enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health points remaining "
     );
@@ -69,8 +73,7 @@ if (confirmSkip) {
 
 };
 
-
-var startGame = function () { 
+ var startGame = function () { 
     playerHealth = 100;
     playerAttack = 10;
     playerMoney = 10;
@@ -82,7 +85,7 @@ var startGame = function () {
 
             var pickedEnemyName = enemyNames[i];
         
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40,60);
         
 
             fight(pickedEnemyName);
@@ -166,5 +169,11 @@ var shop = function(){
     }
 };
 
-startGame();
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
+
+//startGame();
 
